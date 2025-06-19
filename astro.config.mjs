1935cf67b0230119a821bd226fede690efe89b7e
Zeiko <142064919+ZeikoDev@users.noreply.github.com>
2025-06-19 01:48:23 -0500

    Revert "Ft/spanishlanguage (#7)" (#8)
    This reverts commit d6da02048739763b78d141e973aee24ee4d944de.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,21 +9,9 @@ export default defineConfig({
   site: "https://zeiko.dev",
   compressHTML: true,
   vite: {
-    resolve: {
-      alias: {
-        "@": path.resolve("./src"),
-      },
-    },
     build: {
       cssMinify: true,
       minify: true,
     },
   },
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "es"],
-    routing: {
-      prefixDefaultLocale: false
-    }
-  }
 });
